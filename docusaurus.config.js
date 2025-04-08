75771e2fb3c398eb8ebc6b8dac5a11ef00d60a45
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-04-08 09:41:08 +0200

    Reverted change
    We will understand all of this some day

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,13 @@ const config = {
   url: 'https://bsplinex.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/BSplineX-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'BSplineX', // Usually your GitHub org/user name.
   projectName: 'BSplineX-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
